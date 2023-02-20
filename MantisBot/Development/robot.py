b9c9348c872294d89445b6c780ffe8c37b989d08
sweeneyd <dan.sweeney90@gmail.com>
2023-02-19 21:22:44 -0500

    adf

--- MantisBot/Development/robot.py
+++ MantisBot/Development/robot.py
@@ -59,8 +59,6 @@ class MyRobot(MagicRobot):
         """Drivetrain Motor Configuration"""
         self.mainLeft_motor = ComboSparkMax(6, [4,5], inverted=False)
         self.mainRight_motor = ComboSparkMax(2, [1,3], inverted=True)
-        # self.mainLeft_motor = ComboTalonSRX(6, [4,5], inverted=False)
-        # self.mainRight_motor = ComboTalonSRX(2, [1,3], inverted=True)
         
         """Sensor Setups"""
         self.colorSensor = rev.ColorSensorV3(wpilib.I2C.Port.kOnboard)
