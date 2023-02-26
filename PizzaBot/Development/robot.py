37bd6e87541d2f2a98b146e4664da4837a6306ed
sweeneyd <dan.sweeney90@gmail.com>
2023-02-25 20:55:05 -0500

    removed double spacing in robot.py

--- PizzaBot/Development/robot.py
+++ PizzaBot/Development/robot.py
@@ -28,43 +28,28 @@ from wpilib import SmartDashboard
 
 import rev
 import ctre
-
 import photonvision
-
 
 from autonomous.controllerPVAprilTagFollowerTeleop import AprilTagPVControllerTeleop
 
 
 from componentsDrive import ComboTalonSRX, DriveTrainModule, ComboSparkMax
-
 from componentsColor import ColorModule
-
 from componentsIMU import IMUModule
-
 from componentsHMI import HMIModule, FlightStickHMI
-
 from componentsVision import VisionModule
-
 from componentsLimelight import LimelightModule
-
 
 # IntakeConfig = namedtuple("IntakeConfig", ["channelA", "channelB"])
 
 class MyRobot(MagicRobot):
     
-
     drivetrain : DriveTrainModule
-
     color : ColorModule
-
     imu : IMUModule
-
     hmi : HMIModule
-
     vision : VisionModule
-
     limelight : LimelightModule
-    
 
     follow_controller : AprilTagPVControllerTeleop
 
@@ -79,42 +64,26 @@ class MyRobot(MagicRobot):
     def createObjects(self):
 
         """Robot initialization function"""
-        
 
         """Drivetrain Motor Configuration"""
-
-        # self.mainLeft_motor = ComboSparkMax(6, [4,5], inverted=False)
-
-        # self.mainRight_motor = ComboSparkMax(2, [1,3], inverted=True)
-
         self.mainLeft_motor = ComboTalonSRX(6, [4,5], inverted=False)
-
         self.mainRight_motor = ComboTalonSRX(2, [1,3], inverted=True)
-        
 
         """"Grabber Setup"""
-        
 
         """Elevator Setup"""
-        
 
         """Sensor Setups"""
-
         self.colorSensor = rev.ColorSensorV3(wpilib.I2C.Port.kOnboard)
         
 
         """IMU Configuration"""
-
         self.imuSensor = ctre.Pigeon2(11)
 
-
         """Camera Configurtation"""
-
         self.camera = photonvision.PhotonCamera('MSWebCam')
 
-
         """User Controller Configuration"""
-
         self.hmi_interface = FlightStickHMI(0, 1)
         
         pass
@@ -123,7 +92,6 @@ class MyRobot(MagicRobot):
         """Disable Autonomous Lockout of Drivetrain access to the HMI"""
 
         self.drivetrain.disable_autoLockout()
-
         self.drivetrain.resetDistance()
 
         # self.drivetrain.setDistance(-10)
@@ -140,32 +108,21 @@ class MyRobot(MagicRobot):
 
         if self.hmi.is_buttonPressed():
 
-
             if not self.drivetrain.is_lockedout():
-
                 self.drivetrain.enable_autoLockout()
-            
 
             SmartDashboard.putString('Follower Controller State = ', self.follow_controller.current_state.title())
-            
 
             self.follow_controller.engage()
             
 
         # TODO: Put on different button to make robot drive forward set distance
-
         # self.drivetrain.mainLeft_motor.setDistance(10)
-
         # self.drivetrain.mainRight_motor.setDistance(10)
-
         # print(self.drivetrain.mainLeft_motor.getDistance(), self.drivetrain.mainLeft_motor.getDistance())
 
-
         else:   
-
             self.drivetrain.disable_autoLockout()
-        
-
 
 if __name__ == "__main__":
 
