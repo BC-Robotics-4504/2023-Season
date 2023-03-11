24444a0820e6ee93f7cbd39db180d1d1a6e0f531
GooseBucket1 <ndside2007@gmail.com>
2023-03-11 09:28:32 -0500

    componentsLED.py

---
+++ MantisBot/Development/componentsLED.py
@@ -0,0 +1,39 @@
+from wpilib import PWM
+
+def getDuty(ton, k):
+    D = ton*0.001/(k*5.005)
+    return D
+
+
+class LEDModule:
+    def __init__(self, channel, multiplier=1):
+        self.channel=channel
+        self.pwm=PWM(channel)
+        self.setMultipier(multipier)
+    
+    def setMultiplier(multiplier):
+        if multiplier == 1:
+            m=PWM.PeriodMultiplier.kPeriodMultiplier_1X
+
+        elif multiplier == 2:
+            m=PWM.PeriodMultiplier.kPeriodMultiplier_2X
+
+        elif multiplier == 4:
+            m=PWM.PeriodMultiplier.kPeriodMultiplier_4X
+
+        else: 
+            m=PWM.PeriodMultiplier.kPeriodMultiplier_1X
+
+        self.pwm.setPeriodMultiplier(m)
+
+        return False
+
+    def disable():
+        self.pwm
+
+
+
+
+
+
+
