92a0cf83225a0b8e4a57fbe95ee8f95b021300d1
Cash Coffman <116964726+Moneydollar@users.noreply.github.com>
2023-03-14 12:59:58 -0400

    asdf

--- MantisBot/Development/componentsIMU.py
+++ MantisBot/Development/componentsIMU.py
@@ -14,63 +14,31 @@ class IMUModule:
     def __init__(self):
         self.YPR = (0, 0, 0)
         self.imuPID = PIDController(.035, .03, .0002)
-        self.target_yaw = 0
-        self.yaw_tolerance = 0.01
-        self.speed_tolerance = 0.1
-        self.enabled = False
-
-    def resetPID(self):
-        self.imuPID.reset()
-        return False
-
-    def enablePID(self):
-        self.enabled = True
-        return False
-    
-    def disablePID(self):
-        self.enabled = False
-        return False
-
-    def isEnabled(self):
-        return self.enabled
 
     def getYPR(self):
         return self.YPR
-        
+    
 
-    def updateYPR(self):
-        self.YPR = self.imuSensor.getYawPitchRoll()[-1]
-        return False
+
+    PID = None
     
-    def setRelativeTargetYaw(self, target_yaw):
-        self.target_yaw = target_yaw
-        return False
-    
-    def getCurrentYaw(self):
-        return self.getYPR()[0]
-    
-    def getTargetYaw(self):
-        return self.target_yaw
-    
-    def runPID(self, target_heading, tolerance = 5, speed_tolerance = .1):
+    def runPID(self, target_heading, tolerance = 15, speed_tolerance = .1):
         isFinished = False
-
+        yaw = self.getYPR()[1][0]
         # Convert degree heading into usuable form
-        self.targetAngle_rad = self.getYPR() + radians(target_heading)
-
-        yaw = self.getYPR()[0]
+        print(self.YPR)
+        self.targetAngle_rad = yaw + radians(target_heading)
+    
         rotation_speed = self.imuPID.calculate(yaw, self.targetAngle_rad)
         rotation_speed = self.drivetrain.clamp(rotation_speed, -1, 1)
         self.drivetrain.setArcade(0, rotation_speed)
 
         # If angle is reached
-        if abs(yaw - self.targetAngle_rad) <= tolerance and abs(rotation_speed) <= speed_tolerance:
+        if abs(yaw - self.targetAngle_rad) <= radians(tolerance) and abs(rotation_speed) <= speed_tolerance:
             self.imuPID.reset() #TODO: make sure this doesn't break anything
             isFinished = True
         return isFinished
 
     def execute(self):
-        self.updateYPR()
-
-        if self.isEnabled():
-            self.updatePID()
+        self.YPR = self.imuSensor.getYawPitchRoll()
+        pass 
