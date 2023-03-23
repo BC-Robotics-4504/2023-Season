c86ab253a9387694c8997bbbe77404bd71e86993
Cash Coffman <116964726+Moneydollar@users.noreply.github.com>
2023-03-23 19:55:37 -0400

    The **Autonomous** Update

--- MantisBot/SMR_Dev/autonomous/controllerAutonomous.py
+++ MantisBot/SMR_Dev/autonomous/controllerAutonomous.py
@@ -4,6 +4,9 @@ from magicbot import AutonomousStateMachine, state, timed_state
 # from componentsGrabber import GrabberModule as Grabber
 from componentsIMU import IMUModule as IMU
 from componentsDrive import DriveTrainModule
+from componentsElevator import ElevatorModule as Elevator
+from componentsGrabber import GrabberModule as Grabber
+from componentsHMI_xbox import HMIModule as HMI
 
 
 class AutonomousMode(AutonomousStateMachine):
@@ -14,31 +17,81 @@ class AutonomousMode(AutonomousStateMachine):
     # grabber : Grabber
     imu: IMU
     drivetrain : DriveTrainModule
+    elevator : Elevator
+    grabber : Grabber
+    imu: IMU
+    hmi : HMI
 
     position = 0
     
-    # elevator_level = 4
-    # grabber_level = 2
+    elevator_level = 4
+    grabber_level = 2
     drive_distance = -3.2
 
     def start(self):
-        self.engaged = True
         self.engage()
 
-    @state(first = True, must_finish = True)
-    def drive_backwards(self):
-        print('[7] Drive')
-        if self.drivetrain.goToDistance(self.drive_distance):
-            print('    ...completes.')
-            self.next_state('stop')
-        return False
-        
-        
+    @state(first= True, must_finish= True)
+    def clear_bumper(self):
+        self.isEngaged = True
+        isFinished = False
+        if self.elevator.getDistance() >= 0.35:
+            self.next_state_now('actuate_grabber')
+        self.elevator.goToLevel(2)
+
     @state(must_finish=True)
-    def stop(self):
-        print('[8] Complete.')
-        self.drivetrain.setArcade(0,0)
+    def actuate_grabber(self):
+        isElevatorFinished = self.elevator.goToLevel(self.elevator_level)
+        isGrabberFinished = self.grabber.goToLevel(self.grabber_level)
+        if isElevatorFinished and isGrabberFinished:
+            self.next_state_now('actuate_grabber_down')
+            self.isEngaged = False
+    
+    @state(must_finish=True)
+    def actuate_grabber_down(self):
+        isElevatorFinished = self.elevator.goToLevel(0)
+        isGrabberFinished = self.grabber.goToLevel(0)
+        if isElevatorFinished and isGrabberFinished:
+            self.next_state_now('dormant')
+            self.isEngaged = False
+            
+    @state(must_finish = True) 
+    def dormant(self):
         return False
+        
+    # @state(first= True, must_finish= True)
+    # def clear_bumper(self):
+    #     self.isEngaged = True
+    #     isFinished = False
+    #     isElevatorFinished = self.elevator.goToLevel(2)
+    #     isDriveTrainFinished = self.drivetrain.goToDistance(self.drive_distance)
+    #     if isElevatorFinished and isDriveTrainFinished:
+    #         self.next_state_now('actuate_grabber')
+        
+        
+    # @state(must_finish=True)
+    # def actuate_grabber(self):
+    #     isElevatorFinished = self.elevator.goToLevel(4)
+    #     isGrabberFinished = self.grabber.goToLevel(2)
+    #     if isElevatorFinished and isGrabberFinished:
+    #         self.next_state_now('drive_backwards')
+         
+         
+    # @state(must_finish = True)
+    # def drive_backwards(self):
+    #     print('[7] Drive')
+    #     if self.drivetrain.goToDistance(self.drive_distance):
+    #         print('    ...completes.')
+    #         self.next_state('stop')
+    #         self.isEngaged = False
+    #     return False
+        
+        
+    # @state(must_finish=True)
+    # def stop(self):
+    #     print('[8] Complete.')
+    #     self.drivetrain.setArcade(0,0)
+    #     return False
 
     # def scorePosition(self, elevator_level=4, grabber_level=2, drive_distance=-3.):
     #     self.grabber_level = grabber_level
